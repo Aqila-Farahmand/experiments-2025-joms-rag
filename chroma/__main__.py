05d992a8d5189c1eca3e07dd99d065b275e8c0e0
Gianluca Aguzzi <gianluca.aguzzi@unibo.it>
2025-04-28 14:44:40 +0200

    chore: minors on chorma

--- chroma/__main__.py
+++ chroma/__main__.py
@@ -8,6 +8,7 @@ from chromadb import PersistentClient
 from chromadb.api.models.Collection import Collection
 from google import genai
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 from chroma import PATH as CHROMA_PATH, generate_chroma_db
 from documents import PATH as DOCUMENTS_PATH, read_csv, from_pandas_to_list
@@ -17,6 +18,7 @@ DEFAULT_CHUNK_SIZES: Set[int] = {128, 256, 512, 1024}
 DEFAULT_OVERLAP_RATIOS: Set[float] = {0.1, 0.2, 0.3, 0.4, 0.5}
 API_KEY: str = os.getenv("GOOGLE_API_KEY")
 client = genai.Client(api_key=API_KEY)
+#embedding_lm = HuggingFaceEmbedding(model_name="nomic-ai/nomic-embed-text-v1.5", trust_remote_code=True)
 embedding_lm = GoogleGenAIEmbedding(model_name="models/text-embedding-004")
 
 def main(
@@ -40,7 +42,8 @@ def main(
                 overlap=overlap,
                 embedding_lm=embedding_lm,
                 base_path=base_path,
-                force_recreate=False
+                force_recreate=False,
+                #db_name_base="nomadic"
             )
 
     print("✅ All embeddings have been created and saved.")
